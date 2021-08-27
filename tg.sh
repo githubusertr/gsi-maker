7e39744c8fa1198882730c1f4adf25ec80f27bee
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-08-27 10:10:58 +0700

    Create tg.sh

---
+++ tg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
+
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
+
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
+
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+}
+
+tg_post_build() {
+	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
+	-F chat_id="$TG_CHAT_ID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3"
+}
